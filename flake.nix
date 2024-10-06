4392d258a84e8c85ed44d8c3baef99e99c66218c
Madeleine Mortensen <me@maddie.wtf>
2024-10-06 14:56:35 +0100

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "QMK (Quantum Mechanical Keyboard) firmware";
+
+  nixConfig.extra-experimental-features = "nix-command flakes";
+  nixConfig.extra-substituters = "https://nrdxp.cachix.org https://nix-community.cachix.org";
+  nixConfig.extra-trusted-public-keys = "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    } @ inputs:
+    utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          qmk
+        ];
+      };
+    });
+}
